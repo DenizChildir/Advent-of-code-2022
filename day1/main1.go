40cde0fb96d6e9e82f50d3b99789490cb52f3e35
asdfghjkl lkjhgfdsa <denizchildir88@gmail.com>
2024-01-15 16:50:35 -0800

    1

---
+++ day1/main1.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main1() {
+	input, _ := os.Open("input.txt")
+	defer input.Close()
+	xxx, _ := ioutil.ReadAll(input)
+	str := string(xxx)
+	str = strings.ReplaceAll(str, "\r", "x")
+	str = strings.ReplaceAll(str, "\n", "y")
+	groups := strings.Split(str, "xyxy")
+	//fmt.Printf("%q", groups)
+
+	result := make([]int, len(groups))
+	for i, group := range groups {
+		//fmt.Printf("%q \n", group)
+		re := regexp.MustCompile(`\d+`)
+		numbers := re.FindAllString(group, -1)
+		for _, k := range numbers {
+			temp, _ := strconv.Atoi(k)
+			result[i] = result[i] + temp
+		}
+	}
+	sort.Ints(result)
+	fmt.Printf("%v\n", result)
+	//result2:=make([][]int,len(result))
+	//for
+
+	//result2 := [][]int{{1000, 2000, 3000}, {4000}, {5000, 6000}, {7000, 8000, 9000}, {10000}}
+	//c := len(result2)
+	//res := make([]int, c)
+	//for i, _ := range result2 {
+	//	for j, _ := range result2[i] {
+	//		res[i] = res[i] + result2[i][j]
+	//	}
+	//
+	//}
+	//println(res)
+	//fmt.Printf("%v\n", result)
+}
