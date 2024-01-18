e1d08250625549cb216760f26186e6434540f600
asdfghjkl lkjhgfdsa <denizchildir88@gmail.com>
2024-01-17 21:54:49 -0800

    almostDay5

--- main.go
+++ main.go
@@ -5,11 +5,34 @@ import (
 	"io/ioutil"
 	"os"
 	"regexp"
+	"slices"
 	"strconv"
 	"strings"
 )
 
 func main() {
+	ar, size := parse()
+	r := regexp.MustCompile(`(?s)(\d+)`)
+	input, _ := os.Open("input.txt")
+	defer input.Close()
+	xxx, _ := ioutil.ReadAll(input)
+	mv := r.FindAllString(string(xxx), -1)
+	mv2 := make([][]int, len(mv)/3)
+	for i := 0; i < len(mv2); i++ {
+		mv2[i] = make([]int, 3)
+	}
+	counter := 0
+	for i := 0; i < len(mv2); i++ {
+		for j := 0; j < 3; j++ {
+			mv2[i][j], _ = strconv.Atoi(mv[counter])
+			counter++
+		}
+	}
+	move(ar, 3, 1, 9, size)
+	//fmt.Println(ar)
+	_ = ar
+}
+func parse() ([][]string, int) {
 	input2, _ := os.Open("input2.txt")
 	defer input2.Close()
 	xxx, _ := ioutil.ReadAll(input2)
@@ -18,7 +41,6 @@ func main() {
 	re := regexp.MustCompile(`(\d+)`)
 	max2 := re.FindAllString(curStack[len(curStack)-1], -1)
 	max3 := max2[len(max2)-1]
-	//fmt.Println(max3)
 	str := strings.Split(str2, "\n")
 	max4, _ := strconv.Atoi(max3)
 	ar := make([][]string, max4-1)
@@ -42,22 +64,59 @@ func main() {
 			if r.MatchString(ar[i][j]) || ar[i][j] == " " {
 				ar2[i][b] = ar[i][j]
 				b++
-				//fmt.Printf("%q", ar[i][j])
 			}
 		}
 		b = 0
-		println("")
 	}
-	//for i := 0; i < len(ar); i++ {
-	//	for j := 0; j < len(ar[i]); j++ {
-	//
-	//		//if r.MatchString(ar[i][j]) || ar[i][j] == " " {
-	//		//	ar2[i][j] = ar[i][j]
-	//		//}
-	//		fmt.Printf("%q", ar[i][j])
-	//	}
-	//}
-	fmt.Printf("%q", ar2)
-	_ = ar
 
+	_ = ar
+	ret := int(str2[len(str2)-1])
+	return ar2, ret
+}
+func move(arry [][]string, a, b, c, size int) [][]string {
+	x := len(arry)
+	y := len(arry[0])
+	t := make([][]string, x)
+
+	for i := 0; i < len(t); i++ {
+		t[i] = make([]string, y)
+	}
+	for i := 0; i < x; i++ {
+		for j := 0; j < y; j++ {
+			//fmt.Println(arry[j][i])
+			t[j][i] = arry[i][j]
+		}
+	}
+	for i := 0; i < len(t[0]); i++ {
+		temp := t[b-1]
+		if temp[i] == " " {
+			temp = temp[1:]
+			t[b-1] = temp
+		} else {
+			break
+		}
+	}
+
+	temp := t[b-1]
+	temp2 := temp[0:a]
+	//t[c-1] = append(temp2, t[c-1]...)
+	fmt.Println(len(t[c]))
+
+	for i := len(t[c]) - 1; i > -1; i-- {
+		if t[c-1][i] != " " {
+			continue
+		}
+		t[c-1][i] = temp2[0]
+		temp2 = temp2[1:]
+	}
+
+	slices.Reverse(temp2)
+	t[c-1] = append(temp2, t[c-1]...)
+
+	//fmt.Println(temp2)
+	//fmt.Println(t[c-1])
+	t[b-1] = t[b-1][a:]
+	fmt.Println(t)
+
+	return t
 }
